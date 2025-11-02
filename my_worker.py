34813a92e99eeeed2ce2e5d5b9484e0fc2a832f0
Saken Tsukenofu <110395849+sakentsunofu@users.noreply.github.com>
2025-11-02 17:55:55 +0500

    Попробовал добавить CLIP

--- my_worker.py
+++ my_worker.py
@@ -2,6 +2,15 @@
 
 import runpod
 
+import numpy as np
+import torch
+from pkg_resources import packaging
+
+torch_version = torch.__version__
+import clip
+
+clip_models = clip.available_models()
+
 def is_even(job):
 
     job_input = job["input"]
@@ -10,9 +19,14 @@ def is_even(job):
     if not isinstance(the_number, int):
         return {"error": "Silly human, you need to pass an integer."}
 
+    result = ""
     if the_number % 2 == 0:
-        return True
+        result = "Even " + torch_version
+        result += " " + clip_models[0]
+    else:
+        result = "Odd " + torch_version
+        result += " " + clip_models[1]
 
-    return False
+    return {"result": result}
 
 runpod.serverless.start({"handler": is_even})
