3acaea43e135158b0eceb0ecc359bf1272749d0a
Saken Tsukenofu <110395849+sakentsunofu@users.noreply.github.com>
2025-11-02 20:50:12 +0500

    CLIP добавил больше параметров

--- my_worker.py
+++ my_worker.py
@@ -1,32 +1,38 @@
 # my_worker.py
 
 import runpod
-
 import numpy as np
 import torch
 from pkg_resources import packaging
-
-torch_version = torch.__version__
 import clip
 
-clip_models = clip.available_models()
+torch_version = torch.__version__
+
+clip_models = ", ".join(clip.available_models())
+
+model, preprocess = clip.load("ViT-B/32")
+model.cuda().eval()
+input_resolution = model.visual.input_resolution
+context_length = model.context_length
+vocab_size = model.vocab_size
+
+models_info = (
+    f"Input resolution: {input_resolution}\n"
+    f"Context length: {context_length}\n"
+    f"Vocab size: {vocab_size}"
+)
 
 def is_even(job):
-
     job_input = job["input"]
     the_number = job_input["number"]
 
     if not isinstance(the_number, int):
         return {"error": "Silly human, you need to pass an integer."}
 
-    result = ""
     if the_number % 2 == 0:
-        result = "Even " + torch_version
-        result += " " + clip_models[0]
+        result = f"Even {torch_version} {clip_models} {models_info}"
     else:
-        result = "Odd " + torch_version
-        result += " " + clip_models[1]
-
+        result = f"Odd {torch_version} {clip_models} {models_info}"
     return {"result": result}
 
 runpod.serverless.start({"handler": is_even})
